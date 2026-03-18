faa19a1e50a351415929d4d76df2f010af4772bd
rodribp <rodrigobonilla177@gmail.com>
2026-03-17 21:43:56 -0600

    changes

---
+++ src/utils/services.js
@@ -0,0 +1,60 @@
+export const SERVICES = [
+  {
+    title: "Servicios de Metal Mecánica",
+    description:
+      "Soluciones de metal mecánica adaptadas a las necesidades del sector construcción. Fabricamos e instalamos estructuras metálicas de alta calidad, garantizando seguridad, durabilidad y cumplimiento de normas técnicas.",
+    details: [
+      {
+        icon: "structure",
+        description: "Fabricación de estructuras metálicas livianas y pesadas.",
+      },
+      {
+        icon: "welding",
+        description: "Soldadura especializada (MIG, TIG, SMAW).",
+      },
+      {
+        icon: "cut",
+        description: "Corte y doblado de láminas y perfiles.",
+      },
+    ],
+  },
+  {
+    title: "Estructuras, tuberías y maquinado",
+    description:
+      "Servicios integrales en la fabricación e instalación de estructuras metálicas, tuberías industriales y piezas maquinadas, adaptadas a proyectos de alto rendimiento.",
+    details: [
+      {
+        icon: "structure",
+        description: "Fabricación de estructuras metálicas livianas y pesadas.",
+      },
+      {
+        icon: "pipeline",
+        description: "Instalación de tuberías industriales.",
+      },
+      {
+        icon: "gear",
+        description: "Maquinado de componentes metálicos.",
+      },
+    ],
+  },
+  {
+    title: "Electricidad residencial e industrial",
+    description:
+      "Diseñamos, instalamos y damos mantenimiento a sistemas eléctricos para hogares, comercios e industrias, cumpliendo con normativas técnicas y altos estándares de calidad.",
+    details: [
+      {
+        icon: "bolt",
+        description: "Instalaciones eléctricas residenciales e industriales.",
+      },
+      {
+        icon: "braker",
+        description:
+          "Instalación de sistemas de protección y control eléctrico.",
+      },
+      {
+        icon: "tabler",
+        description: "Tableros de distribución y automatización.",
+      },
+    ],
+  },
+];
