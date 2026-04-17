293522bd6cb55e806718e4ebe75b72469f3e3a27
rodribp <rodrigobonilla177@gmail.com>
2026-04-16 22:50:56 -0600

    change email

--- src/utils/global_variables.js
+++ src/utils/global_variables.js
@@ -1,4 +1,4 @@
-export const CONTACT_EMAIL = "rafabonilla@rbcproyectos.com";
+export const CONTACT_EMAIL = "rafaelbonillacontreras@gmail.com";
 export const CONTACT_PHONE = {
   value: "+50370186602",
   string: "+503 7018 6602",
